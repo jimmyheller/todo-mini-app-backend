99c1faaf791a80879da98a3d387330d544b1c31d
Jimmy Heller <ali.tommy.heller2009@gmail.com>
2024-10-15 18:33:23 +0200

    adding logs to troubleshoot

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -39,9 +39,13 @@ router.post('/welcome-token', async (req, res) => {
 router.post('/daily-streak', async (req, res) => {
   try {
     const { telegramId } = req.body;
+    console.log('telegramId', telegramId);
     const user = await checkAndUpdateDailyStreak(telegramId);
+    console.log('user', user);
     res.json(user);
+    console.log('res.json(user)', res.json(user));
   } catch (error) {
+    console.log('exception in daily-streak', error);
     res.status(500).json({ message: 'Error checking daily streak' });
   }
 });
